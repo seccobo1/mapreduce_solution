c6b8de9d2f90dcd4501ba7edbc829520cf6d811d
Elias <brummundelias@gmail.com>
2021-06-01 09:39:05 +0200

    Exercise 1

--- mapper.py
+++ mapper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Import the sys library 
+# Import the sys library
 # for writing and reading the standard input and output
 import sys
 
@@ -22,11 +22,11 @@ import sys
 # Visa		205.96
 # Cash		11.32
 # Cash		444.19
-# Output is tabulator (\t) separated 
+# Output is tabulator (\t) separated
 # New Line (\n) indicates a new record
 # Output is writing to the standard output
 
-# For each new line in the standard input (stdin) 
+# For each new line in the standard input (stdin)
 for line in sys.stdin:
 
     # split the line at the tabulator ("\t")
@@ -38,7 +38,7 @@ for line in sys.stdin:
     date, time, item, category, sales, payment = data
 
     # Write the key-value combination to standard output (stdout)
-    # Key is the payment, value is the sales     
+    # Key is the payment, value is the sales
     # With a tab (\t) between key and value
     # New line \n means new record
-    sys.stdout.write("{0}\t{1}\n".format(payment, sales))
+    sys.stdout.write("{0}\t{1}\n".format(category, sales)
