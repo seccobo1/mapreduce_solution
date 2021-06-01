dbeb0e4a5aca5d1a2b03617874f9b81358bb38a9
Elias <brummundelias@gmail.com>
2021-06-01 11:29:06 +0200

    Exercise 4

--- mapper.py
+++ mapper.py
@@ -40,8 +40,9 @@ for line in sys.stdin:
 	# store the 6 elements of the tuple in seperate variables
 	date, time, item, category, sales, payment = data
 
-	# Write the key-value combination to standard output (stdout)
+	# Write the key-value combination to standard output (stdout), if the category is Computers, Cameras or Video Games
 	# Key is the payment, value is the sales
 	# With a tab (\t) between key and value
 	# New line \n means new record
-	sys.stdout.write("{0}\t{1}\n".format(category, sales))
+	if category == "Computers" or category == "Cameras" or category == "Video Games":
+		sys.stdout.write("{0}\t{1}\n".format(category, sales))
